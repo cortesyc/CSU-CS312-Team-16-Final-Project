7c82c04b8be44d85b0bf5ee7491798a248e4e7a7
so0nho <104328920+so0nho@users.noreply.github.com>
2023-03-29 10:16:44 -0600

    Create generator.php

---
+++ fuel/app/classes/views/generator.php
@@ -0,0 +1,29 @@
+<h1>Color generator</h1>
+
+<?php
+    echo Form::open(array(
+        'action' => 'index/milestone', 
+        'method' => 'get'
+    ));
+
+    echo Form::label('Number of rows and columns:&nbsp;', 'rowsCols');
+    echo Form::input('rowsCols', null, array(
+        'type' => 'number', 
+        'min' => '1', 
+        'max' => '26', 
+        'required' 
+    ));
+    echo '<br>';
+
+    echo Form::label('Number of colors:&nbsp;', 'colors');
+    echo Form::input('colors', null, array(
+        'type' => 'number', 
+        'min' => '1', 
+        'max' => '10', 
+        'required'
+    ));
+    echo '<br>';
+
+    echo Form::submit();
+    echo Form::close();
+?>
