14c065af08c2a7974966ff0befab6433def8aeb5
so0nho <104328920+so0nho@users.noreply.github.com>
2023-03-31 16:51:44 -0600

    Update about.php

--- fuel/app/classes/views/milestone/about.php
+++ fuel/app/classes/views/milestone/about.php
@@ -16,8 +16,10 @@
     and some of the best practices currently in use. I'm excited to graduate this year and enter the industry with the knowledge and experience I've accrued during my time at CSU. </p>
     </div>
     <div class="profile3">
-      <?php echo Asset::img("", array("alt" => "Soonho's Photo", "class" => "profile3"));?>
-      <p><b>Soonho Kwon</b><br> bio </p>
+      <?php echo Asset::img("soonho-profile.jpg", array("alt" => "Soonho's Photo", "class" => "profile3"));?>
+      <p><b>Soonho Kwon</b><br> I am a senior undergraduate Biology major at Colorado State University who is interested in analyzing genetic 
+      data to find relationships between genes that could be the cause for genetic diseases. I am interested in learning web design and development 
+      to have a platform to display analyzed data.</p>
     </div>
     <div class="profile4">
       <?php echo Asset::img("ashaka-p.png", array("alt" => "Ashaka's Photo", "class" => "profile4"));?>
