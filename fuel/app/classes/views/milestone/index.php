d00a420444a9540271efe0560ba61c72568aff9f
Ashaka Pathak <73911711+ashakapathak@users.noreply.github.com>
2023-03-30 19:00:51 -0600

    Update index.php

--- fuel/app/classes/views/milestone/index.php
+++ fuel/app/classes/views/milestone/index.php
@@ -1,2 +1,60 @@
-<h1>Home page</h1>
+.column{
+    float: left;
+    width: 50%;
+}
+img.image{
+    padding-right: 0px;
+    width: 300px;
+    height: 300px;
+    padding-top: 70px;
+    padding-left: 20px;
+}
 
+body {
+    background-color: rgb(219, 224, 219);
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+h1 {
+    margin: 24px;
+}
+
+h3 {
+    padding-left: 24px;
+}
+
+p {
+    padding-left: 24px;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-height: 60px;
+    padding: 0 24px;
+    background-color: #84ab8a;
+}
+
+.navbar {
+    list-style-type: none;
+}
+
+.navbar li {
+    display: inline-block;
+    color:rgb(219, 224, 219);
+}
+
+.navbar a {
+    text-decoration: none;
+    margin: 0 8px;
+    font-weight: bold;
+    color: black;
+}
+
+.navbar a:hover {
+    color: gray;
+}
