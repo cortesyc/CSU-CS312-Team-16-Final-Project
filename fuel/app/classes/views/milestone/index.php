8a2e2a7c3cf539fba558da02c14c252ccbf92a18
Ashaka Pathak <73911711+ashakapathak@users.noreply.github.com>
2023-03-30 19:02:03 -0600

    Update index.php

--- fuel/app/classes/views/milestone/index.php
+++ fuel/app/classes/views/milestone/index.php
@@ -1,60 +1,30 @@
-.column{
-    float: left;
-    width: 50%;
-}
-img.image{
-    padding-right: 0px;
-    width: 300px;
-    height: 300px;
-    padding-top: 70px;
-    padding-left: 20px;
-}
+<body>
+<div class="column">
+<h1>Welcome to Terra</h1>
 
-body {
-    background-color: rgb(219, 224, 219);
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    font-family: Arial, Helvetica, sans-serif;
-}
-
-h1 {
-    margin: 24px;
-}
-
-h3 {
-    padding-left: 24px;
-}
-
-p {
-    padding-left: 24px;
-}
-
-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    max-height: 60px;
-    padding: 0 24px;
-    background-color: #84ab8a;
-}
-
-.navbar {
-    list-style-type: none;
-}
-
-.navbar li {
-    display: inline-block;
-    color:rgb(219, 224, 219);
-}
-
-.navbar a {
-    text-decoration: none;
-    margin: 0 8px;
-    font-weight: bold;
-    color: black;
-}
-
-.navbar a:hover {
-    color: gray;
-}
+<h3>
+    What is Terra?
+</h3>
+<p>
+    Terra is an environmental company dedicated to providing eco-friendly options of everyday products. We pride ourselves on our ability
+    reach out to all corners of the globe in our mission to better our environment. <br>
+</p>
+<h3>
+    Who is Terra?
+</h3>
+<p>
+    To learn more about the brilliant folks that work here, take a look at our about page in the navigation menu. <br>
+</p>
+<h3>
+    Why is Terra?
+</h3>
+<p>
+    Terra exists for the sole purpose of replacing all everyday products in our lives, such as ziploc bags and plastic wrap, with versions
+    that are more friendly towards the environment. This is a simple way for everyday folks to be able to contribute to cleaning up our 
+    planet. 
+</p>
+</div>
+<div class=column>
+<img class="image" src="https://www.clipartmax.com/png/full/1-16901_cartoon-earth-earth-clipart.png" alt="Clean Planet">
+</div>
+</body>
