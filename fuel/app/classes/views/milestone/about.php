77428cabca4e87f53e38953aa3e74810a21c747a
cortesyc <111808526+cortesyc@users.noreply.github.com>
2023-03-31 01:24:30 -0600

    Update about.php
    created about.php for milestone 1

--- fuel/app/classes/views/milestone/about.php
+++ fuel/app/classes/views/milestone/about.php
@@ -1,1 +1,26 @@
-<h1>About Page</h1>
+<body>
+<div class="column">
+<h1>About the Team</h1>
+</div>
+
+  <div class="profiles">
+    <div class="profile1">
+      <?php echo Asset::img("claire-profile.jpg", array("alt" => "Claire's Photo", "class" => "profile1"));?>
+      <p class="bio1"><b>Claire Cortesy</b><br> I am a senior Computer Science Major with a minor in film studies at Colorado State University.
+      Over the summer I had to make a website for a club event. I was limited by the functionality of the web design application I was using 
+      and became frustrated. So I decided to take CS 312 so that if I was ever asked to work on a website again I had the tools to make it look and function exactly as I desired.</p>
+    </div>
+    <div class="profile2">
+      <?php echo Asset::img("", array("alt" => "Michael's Photo", "class" => "profile2"));?>
+      <p><b>Michael Cucina</b><br> bio </p>
+    </div>
+    <div class="profile3">
+      <?php echo Asset::img("", array("alt" => "Soonho's Photo", "class" => "profile3"));?>
+      <p><b>Soonho Kwon</b><br> bio </p>
+    </div>
+    <div class="profile4">
+      <?php echo Asset::img("", array("alt" => "Ashaka's Photo", "class" => "profile4"));?>
+      <p><b>Ashaka Pathak</b><br> bio </p>
+    </div>
+  </div>
+</body>
