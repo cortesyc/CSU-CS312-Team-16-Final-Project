4ec91661790cecf8b1d4f00c7cbffe6fe772098f
so0nho <104328920+so0nho@users.noreply.github.com>
2023-05-04 19:34:52 -0600

    Moved the form to generator.php

--- fuel/app/classes/views/milestone/generator.php
+++ fuel/app/classes/views/milestone/generator.php
@@ -8,7 +8,7 @@
         'method' => 'get'
     ));
 
-    echo Form::label('Number of rows and columns:&nbsp;', 'rowsCols');
+    echo Form::label('Number of rows and columns:&nbsp;', 'rowsCols', array("class" => "label"));
     echo Form::input('rowsCols', null, array(
         'type' => 'number', 
         'min' => '1', 
@@ -17,15 +17,29 @@
     ));
     echo '<br>';
 
-    echo Form::label('Number of colors:&nbsp;', 'colors');
+    echo Form::label("Number of colors (up to $colorCount):&nbsp;", 'colors', array("class" => "label"));
     echo Form::input('colors', null, array(
         'type' => 'number', 
         'min' => '1', 
-        'max' => '10', 
+        'max' => $colorCount,   
         'required'
     ));
     echo '<br>';
 
-    echo Form::submit();
+    echo Form::submit("submit button", "Submit", array("class" => "button"));
     echo Form::close();
 ?>
+
+<form class="db" method="post">
+    <h2>Add color</h2>
+    <label for="color-add"><strong>Name of color to add:</strong></label><br>
+    <input type="text" placeholder="Color to add" name="color-add"></input><br> 
+    <label for="color-add-hex"><strong>Hex value of color to add:</strong></label><br>
+    <input type="text" placeholder="Color hex value" name="color-add-hex"></input><br>
+    <input type="submit" value="Add color" name="add"><br> 
+
+    <h2>Delete color</h2>
+    <label for="color-delete"><strong>Name of color to delete:</strong></label><br>
+    <input type="text" placeholder="Color to delete" name="color-delete"></input><br>
+    <input type="submit" value="Delete color" name="delete"><br> 
+</form>
